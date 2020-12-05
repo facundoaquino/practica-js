6ab4262570f8dfe9d892e40f577faf2027864c07
Facundo Aquino <facun90@live.com>
2020-12-04 21:49:40 -0300

    duplicates

--- combinedSortArray.js
+++ combinedSortArray.js
@@ -1,12 +1,9 @@
 // dado dos array ordenados devolver 1 solo array ordenado , sin duplicados
 
-const combinedArray = (arr1,arr2)=>{
+const combinedArray = (arr1, arr2) => {
+	const result = [...arr1, ...arr2]
 
-    const result = [...arr1 , ...arr2]
-
-
-    return result.sort((a,n)=>a-n)
-
+	return result.sort((a, n) => a - n).filter((el, inx, arr) => arr.indexOf(el) === inx)
 }
 
-console.log(combinedArray([2,3,4,1],[10,3,4]));
+console.log(combinedArray([2, 3, 4, 1], [10, 3, 4]))
