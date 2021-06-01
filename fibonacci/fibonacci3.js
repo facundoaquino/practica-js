86f3bd40fea31ed28fc01a1d7224713b21cb5270
Facundo Aquino <facun90@live.com>
2021-05-31 21:25:46 -0300

    fibonacci 3 , building block from n terms

---
+++ fibonacci/fibonacci3.js
@@ -0,0 +1,22 @@
+//construir n terminos de la secuencia de fibonacci
+
+//1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377....
+
+console.time('fibo interative')
+const getFibonacciUntil = (terms) => {
+	if (terms === 1) return [0]
+	if (terms === 2) return [0, 1]
+
+	const start = [0, 1]
+
+	for (let i = 0; i < terms - 2; i++) {
+		const num = start[i] + start[i + 1]
+		start.push(num)
+	}
+
+	return start
+}
+
+const result = getFibonacciUntil(109000000)
+
+console.timeEnd('fibo interative')
