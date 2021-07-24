9eecc9599b40b73e1be7978b9717226b65abcf7e
Facundo Aquino <facun90@live.com>
2021-07-24 14:32:57 -0300

    suma naterales recursion

---
+++ recursion/sumaNaturales.js
@@ -0,0 +1,29 @@
+const num = 7
+
+console.time('recursion')
+const sumRecursion = (num) => {
+	if (num == 1) return 1
+	if (num == 0) return 0
+
+	return num + sumRecursion(num - 1)
+}
+
+console.log(sumRecursion(num))
+console.timeEnd('recursion')
+
+/*----------------------  ---------------------*/
+// tarda mucho menos la solucion iterativa
+console.time('iterative')
+const iterativeSum = (num) => {
+	let sum = 0
+
+	while (num !== 0) {
+		sum += num
+		num--
+	}
+
+	return sum
+}
+
+console.log(iterativeSum(num))
+console.timeEnd('iterative')
