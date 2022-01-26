2242cffb9d289c764e5f5c81d38578e33d1b8d80
Facundo Aquino <facun90@live.com>
2022-01-26 17:28:02 -0300

    adding methods to linkedList

--- Data-Structure/linkedList.js
+++ Data-Structure/linkedList.js
@@ -1,7 +1,7 @@
-//declaaamos el nodo
+//LISTA A PARTIR DE UN ARRAY AGREGANDO AL FINAL DE LA LISTA , OSEA MANTENIENDO EL ORDEN
 
 class Node {
-	constructor(data, next) {
+	constructor(data) {
 		this.data = data
 		this.next = null
 	}
@@ -15,7 +15,7 @@ class LinkedList {
 
 	//agrega al final , osea que mantiene el orden
 	add(data) {
-		const newNode = new Node(data, null)
+		const newNode = new Node(data)
 		if (!this.head) {
 			this.head = newNode
 		} else {
@@ -26,7 +26,52 @@ class LinkedList {
 			current.next = newNode
 		}
 	}
+
+	// agrega al principio de la lista
+	addBegin(data) {
+		const newNode = new Node(data)
+		newNode.next = this.head
+		this.head = newNode
+	}
+
+	//metodo para imprimir la list
+
+	printList() {
+		let aux = this.head
+		while (aux) {
+			console.log(aux.data)
+			aux = aux.next
+		}
+	}
 }
 
-const myList = new LinkedList()
-console.log(myList)
+// creamos lista
+let myList = new LinkedList()
+
+// Array
+const arr = [1, 23, 5, 10, 3, 43, 332, 4, 55]
+
+// agregamos al final con el metodo add de myList
+arr.forEach((num) => myList.add(num))
+
+// funcion que recibe lista la recorre e imprime , recordar que en js los parametros van por valor no por referencia entonces mantenemos nuestro puntero original apuntando al head de la lista
+
+const showList = (list) => {
+	// console.log(list)
+	while (list) {
+		console.log(list.data)
+		list = list.next
+	}
+}
+
+// showList(myList.head)
+
+/*----------------------  ---------------------*/
+
+const myList2 = new LinkedList()
+
+arr.forEach((num) => myList2.addBegin(num))
+// showList(myList2.head)
+
+myList.printList()
+myList2.printList()
--- main.js
+++
@@ -1,9 +0,0 @@
-// dado un numero intero positivo devolver la suma de sus cif
-console.log('prueba')
-
-const arr1 = [1,2,3,4,5,6]
-
-for (const num of arr1) {
-  console.log(num)
-  
-}
