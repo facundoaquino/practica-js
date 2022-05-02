1f700308616378995b30c035fee69c9fa606ea62
Facundo Aquino <facun90@live.com>
2022-05-02 10:51:08 -0300

    fix: fix minimunSwaps but has not good performance

--- hackerrank/minimumSwaps.js
+++ hackerrank/minimumSwaps.js
@@ -1,6 +1,5 @@
 // You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any two elements. Find the minimum number of swaps required to sort the array in ascending order.
-
-const arr = [1, 3, 2, 4, 5]
+const arr = [3, 4, 6, 7, 8, 5]
 
 function minimumSwaps(arr) {
 	let swapCounter = 0
@@ -10,10 +9,15 @@ function minimumSwaps(arr) {
 
 	for (let i = 0; i < arr.length; i++) {
 		if (!(arr[i] === min + i)) {
+			const swappedOne = arr[i]
+			//FIXME: it´s works but have bad performance fro hacker rank
+			const swappedFromIndex = arr.indexOf(min + i)
+			arr[i] = min + i
+			arr[swappedFromIndex] = swappedOne
 			swapCounter++
 		}
 	}
-	return swapCounter - 1
+	return swapCounter
 }
 
 console.log(minimumSwaps(arr))
