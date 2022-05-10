471a7506f8acf5896e4cb194dd389de044b55f51
Facundo Aquino <facun90@live.com>
2022-05-10 12:23:03 -0300

    hackerRank sockMerchant

---
+++ hackerrank/sockMerchant.js
@@ -0,0 +1,43 @@
+// There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+// Example
+
+// There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+
+// Function Description
+
+// Complete the sockMerchant function in the editor below.
+
+// sockMerchant has the following parameter(s):
+
+// int n: the number of socks in the pile
+// int ar[n]: the colors of each sock
+// Returns
+
+// int: the number of pairs
+// Input Format
+
+// The first line contains an integer , the number of socks represented in .
+// The second line contains  space-separated integers, , the colors of the socks in the pile.
+
+function sockMerchant(n = 9, ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]) {
+	// Write your code here
+	console.log({ n, ar })
+
+	const pairsMap = {}
+	let pairs = 0
+	for (let i = 0; i < ar.length; i++) {
+		if (pairsMap[ar[i]]) {
+			pairsMap[ar[i]] = pairsMap[ar[i]] + 1
+			if (pairsMap[ar[i]] % 2 === 0) {
+				pairs++
+			}
+		} else {
+			pairsMap[ar[i]] = 1
+		}
+	}
+	console.log(pairsMap)
+	console.log(pairs)
+}
+
+sockMerchant()
